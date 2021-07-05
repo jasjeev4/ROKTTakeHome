a4df9fc6c0bd300b93a5347003173184cae0f646
Jasjeev <jasjeev4@gmail.com>
2021-07-05 00:27:26 -0400

    Complete validation tests

--- index.js
+++ index.js
@@ -46,71 +46,13 @@ async function endDateQ() {
     answers["endDate"] = answer["endDate"].trim()
 }
 
-async function askQs() {
+async function begin() {
     let _ = await filePathQ();
+
+    // get the answers
+
+
     console.log(JSON.stringify(answers))
 }
 
-askQs()
-
-function ensureValidCommands(input) {
-    let part = -1
-    let progress = 0
-    const missingCommands = [
-        "Must specify a file path with \"-file <path to file\">",
-        "Must specify a start date with \"-begin <start date>\"",
-        "Must specify a end date with \"-end <end date>\""
-    ]
-    let commands = {}
-
-    for (const word in input) {
-        switch (word) {
-            case "-file":
-                part = 0
-                progress += 1
-                break
-            case "-begin":
-                part = 1
-                progress += 1
-                break
-            case "-end":
-                part = 2
-                progress += 1
-                break
-            default:
-                switch (part) {
-                    case 0:
-                        if (fs.existsSync(word)) {
-                            commands["path"] = word
-                        }
-                        else {
-                            console.log("Invalid file path")
-                            return null
-                        }
-                        break
-                    case 1:
-                        if (word.isValid()) {
-                            commands["begin"] = part
-                        }
-                        else {
-                            console.log("Invalid start date") // add example of valid date
-                            return null
-                        }
-                        break
-                    case 2:
-                        if (word.isValid()) {
-                            commands["begin"] = part
-                        }
-                        else {
-                            console.log("Invalid start date") // add example of valid date
-                        }
-                        break
-                    default:
-                        console.log(missingCommands[progress])
-                        return null
-                }
-                part = -1
-        }
-        return commands
-    }
-}
+begin()
--- tests/validateInputs.test.js
+++ tests/validateInputs.test.js
@@ -14,13 +14,38 @@ describe('Test invalid date times', () => {
 });
 
 describe('Valid date times ', () => {
-    test('Just a year', async () => {
+    test('just a year', async () => {
         let ans = await validators.validateDate("2000");
         expect(ans).toBe(true);
     });
 
     test('UTC time', async () => {
         let ans = await validators.validateDate("2021-07-05T04:16:57Z\n");
+        expect(ans).toBe(true);
+    });
+});
+
+
+describe('Test invalid file path', () => {
+    test('no file path provided', async () => {
+        let ans = await validators.validateFilePath("");
+        expect(ans).toMatch(messages.reenterFile);
+    });
+
+    test('not valid file path', async () => {
+        let ans = await validators.validateFilePath("122");
+        expect(ans).toMatch(messages.reenterFile);
+    });
+});
+
+describe('Valid file paths', () => {
+    test('sample1.txt', async () => {
+        let ans = await validators.validateFilePath("sample1.txt");
+        expect(ans).toBe(true);
+    });
+
+    test('readme.md', async () => {
+        let ans = await validators.validateFilePath("readme.md");
         expect(ans).toBe(true);
     });
 });
